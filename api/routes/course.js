8691125fd9cfe68dc07b6fa563569a6179e22892
asbillings07 <superswiftdeveloper@gmail.com>
2020-01-10 19:43:08 -0500

    condensed courses file

--- api/routes/course.js
+++ api/routes/course.js
@@ -1,182 +1,131 @@
-const express = require('express');
-const router = express.Router();
-const { Course } = require('../models');
-const { User } = require('../models');
-const { authenticateUser } = require('./authenticateUser');
-const { check, validationResult } = require('express-validator');
+const express = require('express')
+const router = express.Router()
+const {
+  deleteCourse,
+  updateCourse,
+  authUser,
+  createCourse,
+  getCourseById,
+  getUserCourses
+} = require('../services/courseController')
+const { authenticateUser } = require('./authenticateUser')
+const { courseValidationChain } = require('./validationChain')
 
 // middleware error handler
-function asyncHandler(cb) {
+function asyncHandler (cb) {
   return async (req, res, next) => {
     try {
-      await cb(req, res, next);
+      await cb(req, res, next)
     } catch (err) {
       if (err === 'SequelizeDatabaseError') {
-        res.status(err.status).json({ error: err.message });
+        res.status(err.status).json({ error: err.message })
       } else {
-        next(err);
+        next(err)
       }
     }
-  };
-}
-const valdationMiddleWare = async (req, res, next) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    const errorMessages = errors.array().map(err => err.msg);
-    res.status(400).json({ errors: errorMessages });
-  } else {
-    next();
   }
-};
-// validate course information
-const courseValidationChain = [
-  check('title')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a Course Title'),
-  check('description')
-    .exists({ checkNull: true, checkFalsy: true })
-    .withMessage('Please provide a Course Description'),
-  valdationMiddleWare,
-];
+}
 
 // Course Routes
 
-//GET /courses 200 - Returns a list of courses (including the user that owns each course)
+// GET /courses 200 - Returns a list of courses (including the user that owns each course)
 router.get(
   '/courses',
 
   asyncHandler(async (req, res, next) => {
-    const courses = await Course.findAll({
-      include: [
-        {
-          model: User,
-          as: 'user',
-          attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
-        },
-      ],
-      attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
-    });
+    const courses = await getUserCourses()
     if (courses) {
-      res.status(200).json(courses);
+      res.status(200).json(courses)
     } else {
       res.status(404).json({
         error: '404 Not Found',
-        message: 'Courses not found at selected route',
-      });
+        message: 'Courses not found at selected route'
+      })
     }
   })
-);
+)
 
-//GET /courses/:id 200 - Returns a the course (including the user that owns the course) for the provided course ID
+// GET /courses/:id 200 - Returns a the course (including the user that owns the course) for the provided course ID
 router.get(
   '/courses/:id',
 
   asyncHandler(async (req, res, next) => {
-    const id = +req.params.id;
+    const id = +req.params.id
 
     if (id) {
-      const course = await Course.findAll({
-        where: {
-          id,
-        },
-        include: [
-          {
-            model: User,
-            as: 'user',
-            attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
-          },
-        ],
-        attributes: { exclude: ['password', 'createdAt', 'updatedAt'] },
-      });
-
-      res.status(200).json(course);
+      const course = await getCourseById(id)
+      res.status(200).json(course)
     } else {
       res.status(404).json({
         error: '404 Not Found',
-        message: 'Course not found at selected Route',
-      });
+        message: 'Course not found at selected Route'
+      })
     }
   })
-);
+)
 
-//POST /courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
+// POST /courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
 router.post(
   '/courses',
 
   authenticateUser,
   courseValidationChain,
   asyncHandler(async (req, res, next) => {
-    const user = req.currentUser;
-    const course = req.body;
-    const courses = await Course.create({
-      userId: user.id,
-      title: course.title,
-      description: course.description,
-      estimatedTime: course.estimatedTime,
-      materialsNeeded: course.materialsNeeded,
-    });
+    const user = req.currentUser
+    const course = req.body
+    const courses = await createCourse(user, course)
     res
       .location(`/courses/:${courses.id}`)
       .status(201)
-      .end();
+      .end()
   })
-);
-//PUT /courses/:id 204 - Updates a course and returns no content
+)
+// PUT /courses/:id 204 - Updates a course and returns no content
 router.put(
   '/courses/:id',
 
   authenticateUser,
   courseValidationChain,
   asyncHandler(async (req, res, next) => {
-    const user = req.currentUser;
-    const course = req.body;
-    const id = +req.params.id;
-    const verifyUser = await Course.findOne({ where: { id } });
+    const user = req.currentUser
+    const id = +req.params.id
+    const verifyUser = await authUser(id)
     if (verifyUser.userId === user.id) {
-      const courses = await Course.findByPk(id);
-      courses.update({
-        title: course.title,
-        description: course.description,
-        estimatedTime: course.estimatedTime,
-        materialsNeeded: course.materialsNeeded,
-      });
-      res.status(204).end();
+      await updateCourse(id)
+      res.status(204).end()
     } else {
       res.status(403).json({
         errors: {
           message:
-            'You can only edit courses that you own. Please choose a choose a course you own and try again.',
-        },
-      });
+            'You can only edit courses that you own. Please choose a choose a course you own and try again.'
+        }
+      })
     }
   })
-);
-//DELETE - courses/:id 204 - deletes a course. Careful, this can not be undone. Deletes a course and returns no content
+)
+// DELETE - courses/:id 204 - deletes a course. Careful, this can not be undone. Deletes a course and returns no content
 router.delete(
   '/courses/:id',
 
   authenticateUser,
   asyncHandler(async (req, res, next) => {
-    const user = req.currentUser;
-    const id = +req.params.id;
-    const verifyUser = await Course.findOne({ where: { id } });
+    const user = req.currentUser
+    const id = +req.params.id
+    const verifyUser = await authUser(id)
     if (verifyUser.userId === user.id) {
-      const course = await Course.findByPk(id);
-
-      course.destroy();
-      res.status(204).end();
+      await deleteCourse(id)
+      res.status(204).end()
     } else {
       res.status(403).json({
         errors: {
           message:
-            'You can only delete courses that you own. Please choose a choose a course you own and try again.',
-        },
-      });
+            'You can only delete courses that you own. Please choose a choose a course you own and try again.'
+        }
+      })
     }
   })
-);
-module.exports = router;
+)
+module.exports = router
 
 /**
  * The PUT /api/courses/:id and DELETE /api/courses/:id routes return a 403 status code if the current user doesn't own the requested course
