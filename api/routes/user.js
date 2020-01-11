7f3f5194823cc28b58b544f6031342ee4e23be2c
asbillings07 <superswiftdeveloper@gmail.com>
2020-01-10 19:43:21 -0500

    condensed user file

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,62 +1,42 @@
-const { validationChain } = require('./validationChain');
-const { authenticateUser } = require('./authenticateUser');
-const express = require('express');
-const router = express.Router();
-const { User } = require('../models');
-const { validationResult } = require('express-validator');
-const bcryptjs = require('bcryptjs');
+const { userValidationChain } = require('./validationChain')
+const { authenticateUser } = require('./authenticateUser')
+const { getUser, createUser } = require('../services/userController')
+const express = require('express')
+const router = express.Router()
 
 // middleware error handler
 
-function asyncHandler(cb) {
+function asyncHandler (cb) {
   return async (req, res, next) => {
     try {
-      await cb(req, res, next);
+      await cb(req, res, next)
     } catch (err) {
-      next(err);
+      next(err)
     }
-  };
+  }
 }
 
 // User Routes
-//GET /api/users 200 - Returns the currently authenticated user
+// GET /api/users 200 - Returns the currently authenticated user
 
 router.get('/users', authenticateUser, (req, res, next) => {
-  const user = req.currentUser;
+  const user = req.currentUser
+  const authUser = getUser(user)
+  res.status(200).json(authUser)
+})
 
-  res.status(200).json({
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    emailAddress: user.emailAddress,
-  });
-});
-
-//POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
+// POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
 router.post(
   '/users',
-  validationChain,
+  userValidationChain,
   asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map(error => error.msg);
-      res.status(400).json({ errors: errorMessages });
-    } else {
-      const user = req.body;
-      user.password = bcryptjs.hashSync(user.password);
-      await User.create({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        emailAddress: user.emailAddress,
-        password: user.password,
-      });
-      res
-        .location('/')
-        .status(201)
-        .end();
-    }
+    const user = req.body
+    const createdUser = await createUser(user)
+    res
+      .location('/')
+      .status(201)
+      .json(createdUser)
   })
-);
+)
 
-module.exports = router;
+module.exports = router
